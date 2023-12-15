3ce2a4c0d13f21731c89a851d50b8dd2638f784c
HOXY S <methodfunc@HOXYui-MacBookPro.local>
2023-12-15 20:26:03 +0900

    setting test

--- settings.py
+++ settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Django Apps
-    'debug_toolbar'
+    'debug_toolbar',
+    'bootstrap5'
 ]
 
 MIDDLEWARE = [
